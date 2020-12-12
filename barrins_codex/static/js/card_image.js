7fcacd62ce1eaf8f2ea4dcf0e7c16660f95bbd6b
Spigushe <martin.cuchet@gmail.com>
2020-12-12 19:09:06 +0100

    Initial Commit
    Need to work on card_image

--- barrins_codex/static/js/card_image.js
+++ barrins_codex/static/js/card_image.js
@@ -1,6 +1,5 @@
 var ajaxRequest;
 function ajaxFunction() {
-
 	try {
 		// Opera 8.0+, Firefox, Safari
 		ajaxRequest = new XMLHttpRequest();
@@ -20,24 +19,26 @@ function ajaxFunction() {
 	}
 }
 
-function get_card_image(name) {
+function get_card_image(name, target, size="normal") {
+	// Initiate AJAX function
 	ajaxFunction();
 
 	// Here processRequest() is the callback function.
-	ajaxRequest.onreadystatechange = process_card_json;
+	ajaxRequest.onreadystatechange = function () {
+		if (ajaxRequest.readyState == 4) { // The request is completed
+			if (ajaxRequest.status == 200) { // HTTP OK
+				json = JSON.parse(ajaxRequest.responseText);
+				if (size === "normal") {
+					document.getElementById(target).src = json.image_uris.normal;
+				}else if (size === "small") {
+					document.getElementById(target).src = json.image_uris.small;
+				}
+			}
+		}
+	}
 
 	var url = "https://api.scryfall.com/cards/named?fuzzy=" + escape(name);
 
 	ajaxRequest.open("GET", url, true);
 	ajaxRequest.send(null);
-}
-
-function process_card_json() {
-	if (ajaxRequest.readyState == 4) { // The request is completed
-		if (ajaxRequest.status == 200) { // HTTP OK
-			json = JSON.parse(ajaxRequest.responseText);
-			document.getElementById("card-image").src = json.image_uris.normal;
-			document.getElementById("card-hover-image").src = json.image_uris.normal;
-		}
-	}
 }
--- barrins_codex/static/js/dC.js
+++ barrins_codex/static/js/dC.js
@@ -1,5 +1,5 @@
 function dC(name) {
-	get_card_image(name);
+	get_card_image(name, "card-image");
 	document.getElementById("card-modal").style.display = "block";
 }
 
@@ -7,10 +7,15 @@ function hC(name) {
 	if (window.matchMedia("(hover: none)").matches) {
 		return
 	}
-	get_card_image(name);
+	get_card_image(name, "card-hover-image");
 	document.getElementById("card-hover").style.display = "block";
 }
 
 function oC() {
 	document.getElementById("card-hover").style.display = "none";
+}
+
+function sC(name, target) {
+	console.log("Chargement de l'image : "+name);
+	get_card_image(name, target, "small");
 }
