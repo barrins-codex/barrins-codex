5508ac5223fa28bade42853a6de568324dfb4b18
Spigushe <martin.cuchet@gmail.com>
2020-12-18 23:30:01 +0100

    Correctif: Affichage des cartes

--- barrins_codex/static/js/dC.js
+++ barrins_codex/static/js/dC.js
@@ -1,21 +1,16 @@
-function dC(name) {
-	get_card_image(name, "card-image");
+function dC(scryfallId) {
+	document.getElementById("card-image").src = "https://api.scryfall.com/cards/"+scryfallId+"?format=image";
 	document.getElementById("card-modal").style.display = "block";
 }
 
-function hC(name) {
+function hC(scryfallId) {
 	if (window.matchMedia("(hover: none)").matches) {
 		return
 	}
-	get_card_image(name, "card-hover-image");
+	document.getElementById("card-hover-image").src = "https://api.scryfall.com/cards/"+scryfallId+"?format=image";
 	document.getElementById("card-hover").style.display = "block";
 }
 
 function oC() {
 	document.getElementById("card-hover").style.display = "none";
-}
-
-function sC(name, target) {
-	console.log("Chargement de l'image : "+name);
-	get_card_image(name, target, "small");
 }
