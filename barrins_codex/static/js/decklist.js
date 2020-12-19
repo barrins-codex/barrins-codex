18ef591751e685c156387e58641caf8c58c25107
Spigushe <martin.cuchet@gmail.com>
2020-12-20 00:40:50 +0100

    Display decklist
    And add an entry to archetypes

--- barrins_codex/static/js/decklist.js
+++ barrins_codex/static/js/decklist.js
@@ -1,45 +1,8 @@
-function dC(name) {
-	document.getElementById("card-image").src = "https://images.krcg.org/".concat(
-		name,
-		".jpg"
-	)
-	document.getElementById("card-prev").style.display = "none"
-	document.getElementById("card-next").style.display = "none"
-	document.getElementById("card-modal").style.display = "block"
+function dCi(scryfallId,i) {
+	document.getElementById("card-image").src = "https://api.scryfall.com/cards/"+scryfallId+"?format=image";
 }
-function hC(name) {
-	if (window.matchMedia("(hover: none)").matches) {
-		return
-	}
-	document.getElementById("card-hover-image").src = 'https://images.krcg.org/'.concat(name, '.jpg');
-	document.getElementById("card-hover").style.display = "block";
-}
-function oC() {
-	document.getElementById("card-hover").style.display = "none";
-}
-function fname(i) {
-	const card = document.getElementById(`card-${i}`)
-	if (card === undefined) {
-		return
-	}
-	var name = card.textContent.toLowerCase()
-	if (name.startsWith("the ")) {
-		name = name.substr(4, name.length) + "the"
-	}
-	name = name.replace(/\s|,|\.|-|—|'|:|\(|\)|"|\/|!/g, "")
-	name = name.replace(/ö|ó/g, "o") // Rötschreck, Dónal
-	name = name.replace(/é|ë|è/g, "e") // Céleste, Gaël, Père
-	name = name.replace(/œ/g, "oe") // Cœur
-	name = name.replace(/ç/g, "c") // Monçada
-	name = name.replace(/á|ã/g, "a") // Vásquez, João
-	name = name.replace(/í|î/g, "i") // Día, Maître
-	name = name.replace(/ñ/g, "n") // Montaña
-	name = name.replace(/ü|ú/g, "u") // Powerbase: Zürich, Jesús
-	name = name.replace(/™/g, "tm") // Pentex™
-	return name
-}
-function dCi(i) {
-	document.getElementById(`card-image`).src = "https://images.krcg.org/".concat(fname(i), '.jpg')
+function dCi(scryfallId,i) {
+	document.getElementById("card-image").src = "https://api.scryfall.com/cards/"+scryfallId+"?format=image";
 	var modal = document.getElementById("card-modal")
 	for (const c of modal.classList) {
 		if (c.startsWith("modal-card-")) {
@@ -79,7 +42,7 @@ function modalKeydown(event) {
 	}
 }
 function cardElement(element, i) {
-	return `<li>${element.count} <span class="card" id="card-${i}" onclick="dCi(${i})" onmouseover="hC(fname(${i}))" onmouseout="oC()">${element.name}</span></li>`
+	return `<li>${element.count} <span class="card" id="card-${i}" onclick="dCi('${element.id}',${i})" onmouseover="hC('${element.id}')" onmouseout="oC()">${element.name}</span></li>`
 }
 function wrapText(text, maxlen) {
 	if (!text) {
@@ -98,18 +61,10 @@ function removeComments() {
 }
 function displayDeck(data, deckname=undefined) {
 	removeComments()
-	document.getElementById("deck-link").textContent = wrapText(
+	document.getElementById("deck-name").textContent = wrapText(
 		deckname || data.name || "(No Name)",
 		25
 	)
-	if (data.twda_id) {
-		document.getElementById(
-			"deck-link"
-		).href = `http://www.vekn.fr/decks/twd.htm#${data["twda_id"]}`
-	}
-	else if (data.link) {
-		document.getElementById("deck-link").href = data.link
-	}
 	header_lines = []
 	if (data.player || data.author) {
 		header_lines.push(wrapText(data.player || data.author, 40))
@@ -127,14 +82,12 @@ function displayDeck(data, deckname=undefined) {
 		header_lines.push(wrapText(data.players_count, 32) + " players")
 	}
 	document.getElementById("deck-header").innerHTML = header_lines.join("<br/>")
-	document.getElementById("crypt-header").textContent = `Crypt (${data.crypt.count})`
 	var cards = []
-	data.crypt.cards.forEach((value, index) => {
+	data.command.cards.forEach((value, index) => {
 		cards.push(cardElement(value, index))
 	})
-	document.getElementById("crypt-list").innerHTML = cards.join("\n")
-	document.getElementById(
-		"library-header").textContent = `Library (${data.library.count})`
+	document.getElementById("commanders").innerHTML = cards.join("\n")
+	document.getElementById("library-header").textContent = `Library`
 	var offset = cards.length
 	var cards = new Array()
 	for (const section of data.library.cards) {
@@ -147,14 +100,5 @@ function displayDeck(data, deckname=undefined) {
 		offset += section.cards.length
 	}
 	document.getElementById("library-list").innerHTML = cards.join("\n")
-	comments = document.getElementById("comments")
-	if (comments && data.comments) {
-		for (let section of data.comments.split("\n\n")) {
-			pelem = document.createElement("p")
-			pelem.textContent = section
-			comments.appendChild(pelem)
-		}
-		document.getElementById("comments-title").style.display = "block"
-	}
 	document.getElementById("decklist").style.display = "block"
 }
