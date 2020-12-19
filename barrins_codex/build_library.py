967371e6f9b7224c091bf053fdd4a67722160595
Spigushe <martin.cuchet@gmail.com>
2020-12-20 00:23:40 +0100

    Decklist converter

--- barrins_codex/build_library.py
+++ barrins_codex/build_library.py
@@ -7,15 +7,20 @@ import re
 
 
 def _name(card):
-	name = card["name"]
+	if "name" in card:
+		name = card["name"]
+	else:
+		name = card
 	name = unidecode.unidecode(name).lower()
 	name = re.sub(r"[^a-zA-Z0-9]", "", name)
 	return name
 
 
-def _get(card,set):
+def _get(card,set,name=None):
+	if name is not None:
+		name = card["name"]
 	return {
-		"name":card["name"],
+		"name":name or card["name"],
 		"date":set["releaseDate"],
 		"id":card["identifiers"]["scryfallId"],
 		"types":card["types"],
@@ -33,25 +38,42 @@ if __name__ == "__main__":
 
 	library = {}
 	for set in sets["data"]:
-		if set["isOnlineOnly"]:
-			# Only images of paper versions
-			pass
-		if set["type"] in SKIP_TYPES:
-			# Only regular expansions
-			pass
-		if len(set["code"]) == 3:
-			# Only regular sets
-			for card in prints["data"][set["code"]]["cards"]:
-				# Cards may have another face : name_face_a / name_face_b
-				if "/" in card["name"]:
-					card["name"] = card["name"].split(" /")[0]
-				# Adding it to library
-				if _name(card) not in library:
-					library[_name(card)] = {_name(card):_get(card, set)}
-				if _name(card) in library:
-					# If newer version exists
-					if set["releaseDate"] > library[_name(card)][_name(card)]["date"]:
-						library[_name(card)] = {_name(card):_get(card, set)}
+		if not set["isOnlineOnly"]:
+			if set["type"] not in SKIP_TYPES:
+				if len(set["code"]) == 3:
+					# Only regular sets
+					for card in prints["data"][set["code"]]["cards"]:
+						# Adding it to library
+						if _name(card) not in library:
+							library[_name(card)] = {_name(card):_get(card, set)}
+						if _name(card) in library:
+							# If newer version exists
+							if set["releaseDate"] > library[_name(card)][_name(card)]["date"]:
+								library[_name(card)] = {_name(card):_get(card, set)}
+						# Cards may have another face : name_face_a / name_face_b
+						# Cards with other faces have a "//" separator
+						if "//" in card["name"]:
+							# Specific handling
+							card_faces = card["name"].split(" // ")
+							face_a = card_faces[0]
+							face_b = card_faces[1]
+							# Cards are already in the library
+							if (_name(face_a) or _name(face_b)) in library:
+								# Need to check types for any new type
+								list_types = library[_name(card)][_name(card)]["types"]
+								# library[_name(card)] if used above and will be updated
+								for t in card["types"]:
+									if t not in list_types:
+										list_types.append(t)
+								# Adding full set of types to card and all faces
+								library[_name(face_a)][_name(face_a)]["types"] = list_types
+								library[_name(face_b)][_name(face_b)]["types"] = list_types
+							# Adding Face A to library
+							if _name(face_a) not in library:
+								library[_name(face_a)] = {_name(face_a):_get(card, set, face_a)}
+							# Adding Face B to library
+							if _name(face_b) not in library:
+								library[_name(face_b)] = {_name(face_b):_get(card, set, face_b)}
 
 
 	if not os.path.isdir(DIR_NAME):
