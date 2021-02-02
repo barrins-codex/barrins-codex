349d0c267bf1512ac42b67e3455b7bc13b3b89cf
Spigushe <martin.cuchet@gmail.com>
2021-02-02 13:17:00 +0100

    Modification de l'affichage des decklists

--- barrins_codex/static/js/decklist.js
+++ barrins_codex/static/js/decklist.js
@@ -1,95 +1,67 @@
-function dCi_id(i) {
-	// Get card-i
-	var card = document.getElementById(`card-${i}`)
-	// Get scryfallId
-	for (const c of card.classList) {
-		if (c.startsWith("scryfall-")) {
-			dCi(c.split("scryfall-")[1], i)
-		}
-	}
-}
-function dCi(scryfallId,i) {
-	document.getElementById("card-image-decklist").src = "https://api.scryfall.com/cards/"+scryfallId+"?format=image";
-
-	var modal = document.getElementById("card-decklist")
-	// Remove previous card info
-	for (const c of modal.classList) {
-		if (c.startsWith("modal-card-")) { modal.classList.remove(c) }
-	}
-	// Set current card info
-	modal.classList.add(`modal-card-${i}`)
-
-	// No before for first card
-	if (i > 0) { document.getElementById("card-prev").style.display = "block" }
-	else { document.getElementById("card-prev").style.display = "none" }
-	// No after for last card
-	if (document.getElementById("card-"+(i+1)) !== null) { document.getElementById("card-next").style.display = "block" }
-	else { document.getElementById("card-next").style.display = "none" }
-
-	// Display card
-	modal.style.display = "block"
-	modal.focus()
-}
-function cardElement(element, i) {
+function cardElement (element) {
 	return `<li>${element.count} <span class="card" scryfallId="${element.id}" data-tippy-content="
 		<div class='card-container'>
-			<img data-src='https://api.scryfall.com/cards/${element.id}?format=image' class='card-image'>
+			<img data-src='https://api.scryfall.com/cards/${element.id}?format=image' src='https://api.scryfall.com/cards/${element.id}?format=image' class='card-image'>
 		</div>" >${element.name}</span></li>`
 }
-function wrapText(text, maxlen) {
-	if (!text) {
-		return "(N/A)"
-	}
+
+function wrapText (text, maxlen) {
+	if (!text) { return "(N/A)" }
+
 	if (text.length > maxlen) {
 		return text.substr(0, maxlen - 3) + "..."
 	}
 	return text
 }
-function removeComments() {
-	var comments = document.getElementById("comments")
-	if (comments) {
-		comments.innerHTML = ""
-	}
-}
-function displayDeck(data, deckname=undefined) {
-	removeComments()
+
+function setName (data) {
 	document.getElementById("deck-name").textContent = wrapText(
-		deckname || data.name || "(No Name)",
+		data.name || "(No Name)",
 		50
 	)
+}
+
+function setHeader (data) {
 	header_lines = []
 	if (data.player || data.author) {
-		header_lines.push(wrapText(data.player || data.author, 40))
-	}
-	if (data.event) {
-		header_lines.push(wrapText(data.event, 40))
-	}
-	if (data.place) {
-		header_lines.push(wrapText(data.place, 40))
-	}
-	if (data.date) {
-		header_lines.push(wrapText(data.date, 40))
-	}
-	if (data.players_count) {
-		header_lines.push(wrapText(data.players_count, 32) + " players")
+		header_lines.push(wrapText(data.player || data.author, 40));
 	}
 	document.getElementById("deck-header").innerHTML = header_lines.join("<br/>")
+}
+
+function setCommanders (data) {
 	var cards = []
 	data.command.cards.forEach((value, index) => {
 		cards.push(cardElement(value, index))
 	})
 	document.getElementById("commanders").innerHTML = cards.join("\n")
-	document.getElementById("library-header").textContent = `Library`
-	var offset = cards.length
-	var cards = new Array()
+}
+
+function setLibrary (data) {
+	var library = document.getElementById('library-container')
 	for (const section of data.library.cards) {
-		cards.push(
-			`<li><h4>— ${section.type} (${section.count}) —</h4></li>`
-		)
+		// Create container for current section
+		var divSection = document.createElement("div")
+		// Create title for current section
+		var title =  document.createElement("h4");
+		title.innerHTML = `— ${section.type} (${section.count}) —`
+		divSection.appendChild(title)
+		// Create cardlist for current section
+		var cardlist = document.createElement("ul")
+		var cards = new Array()
 		section.cards.forEach((value, index) => {
-			cards.push(cardElement(value, offset + index))
+			cards.push(cardElement(value))
 		})
-		offset += section.cards.length
+		cardlist.innerHTML = cards.join("\n")
+		divSection.appendChild(cardlist)
+		// Push section into decklist
+		library.appendChild(divSection)
 	}
-	document.getElementById("library-list").innerHTML = cards.join("\n")
+}
+
+function displayDeck (data) {
+	setName(data);
+	setHeader(data);
+	setCommanders(data);
+	setLibrary(data);
 }
